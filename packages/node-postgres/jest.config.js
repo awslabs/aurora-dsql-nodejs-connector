dd7f4b7d6eed8cc5706df0e292a637aa389928e1
Mitchell Elholm <elholmit@amazon.com>
2025-11-26 12:43:01 -0800

    update jest config

--- packages/node-postgres/jest.config.js
+++ packages/node-postgres/jest.config.js
@@ -5,6 +5,9 @@
 export default {
   preset: 'ts-jest/presets/default-esm',
   extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   globals: {
     'ts-jest': {
       useESM: true
