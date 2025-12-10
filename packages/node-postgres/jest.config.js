f61ae9479bc34df4659109b7dcc2e5c40f2550b6
Daniel Frankcom <daniel@frankcom.ca>
2025-12-10 10:38:03 -0800

    Export node-postgres CommonJS module in addition to ES module (#92)
    This PR adds a bundling step to the build, which produces a CommonJS
    module in addition to the current ES module.
    
    Without this, users that have a CommonJS package will get an error like
    this when trying to import the package:
    
    ```
    src/index.ts:1:34 - error TS1479: The current file is a CommonJS module whose imports will produce 'require' calls; however, the referenced file is an ECMAScript module and cannot be imported with 'require'. Consider writing a dynamic 'import("@aws/aurora-dsql-node-postgres-connector")' call instead.
      To convert this file to an ECMAScript module, change its file extension to '.mts' or create a local package.json file with `{ "type": "module" }`.
    
    1 import { AuroraDSQLClient } from "@aws/aurora-dsql-node-postgres-connector";
                                       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    Found 1 error in src/index.ts:1
    ```
    
    While it is still possible to import the package with workarounds, it
    adds a layer of friction for package adoption.
    
    By adding `tsdown`, we can now run a bundling step which produces both
    CommonJS and ES module files for the package.
    
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

--- packages/node-postgres/jest.config.js
+++ packages/node-postgres/jest.config.js
@@ -8,10 +8,11 @@ export default {
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  globals: {
-    'ts-jest': {
-      useESM: true
-    }
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+      tsconfig: 'tsconfig.json',
+    }],
   },
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
